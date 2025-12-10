670859180efee461e6260fcbed39e4b9308b09c7
Hanna Torianyk <an.mol4anova@gmail.com>
2025-12-10 13:37:11 +0200

    Fixed the errors

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -11,25 +11,31 @@ function transformStateWithClones(state, actions) {
   let currentState = { ...state };
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      currentState = {};
+    let nextState = { ...currentState };
+
+    switch (action.type) {
+      case 'addProperties':
+        nextState = { ...nextState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        nextState = Object.fromEntries(
+          Object.entries(nextState).filter(
+            ([key]) => !action.keysToRemove.includes(key),
+          ),
+        );
+        break;
+
+      case 'clear':
+        nextState = {};
+        break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
-    if (action.type === 'addProperties') {
-      currentState = { ...currentState, ...action.extraData };
-    }
-
-    if (action.type === 'removeProperties') {
-      const newState = { ...currentState };
-
-      for (const key of action.keysToRemove) {
-        delete newState[key];
-      }
-
-      currentState = newState;
-    }
-
-    resultStates.push(currentState);
+    resultStates.push(nextState);
+    currentState = nextState;
   }
 
   return resultStates;
